dd606e6b74a0d10f833e13827b89af4d78246fd5
Akshay Bhatia <akshaybhatia10@gmail.com>
2016-11-12 06:26:21 +0000

    Final changes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require("express"),
+    multer  = require("multer"),
+    upload  = multer(),
     app     = express();
     
 app.set("view engine", "ejs");
@@ -7,6 +9,14 @@ app.get("/",function(req,res){
     res.render("index");
 });
 
+app.get("/upload",function(req,res){
+    res.redirect("/");
+});
 
+app.post("/upload",upload.single("file"),function(req,res){
+    res.json({name: req.file.originalname , size : req.file.size });
+    
+    
+});
     
 app.listen(process.env.PORT, process.env.IP);
